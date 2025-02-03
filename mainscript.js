a915989c91c559d7101c6b0f4462d9e6ed76e04d
Boxel <128452230+BoxelLogica@users.noreply.github.com>
2025-02-03 11:27:09 -0700

    please work stupid paths

--- mainscript.js
+++ mainscript.js
@@ -63,17 +63,17 @@ function preload ()  {
 
     // SOPHIE SPRITESHEET
     this.load.spritesheet('sophie', 
-        '/sprites/sophie.png', 
+        './sprites/sophie.png', 
          { frameWidth: 20, frameHeight: 24 }
     )
     // PLAYERTEST SPRITESHEET
     this.load.spritesheet('playerTest', 
-        '/sprites/green_test_sprite.png', 
+        './sprites/green_test_sprite.png', 
          { frameWidth: 32, frameHeight: 32 }
     )
     // PLAYERTEST MISSINGTEXTURE
     this.load.spritesheet('missingTexture', 
-        '/sprites/missingTexture.png', 
+        './sprites/missingTexture.png', 
          { frameWidth: 32, frameHeight: 32 }
     )
 }
